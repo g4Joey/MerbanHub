08dc44a2e09a3f76d39712bd156d635fc53c89b4
Lucius <luciusjackson5@gmail.com>
2025-06-24 19:52:16 +0000

    setup next js frontend

---
+++ frontend/app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Button} from "@/components/ui/button";
+
+const Page = () => {
+    return (
+        <div className={"font-black m-2 flex"}>
+          hello
+            <Button className={"bg-amber-900"}>hello</Button>
+        </div>
+    );
+};
+
+export default Page;
