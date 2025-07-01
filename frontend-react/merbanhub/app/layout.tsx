d580dc2dba7da976d2625aecd6b1b6e084b2f792
Lucius <luciusjackson5@gmail.com>
2025-07-01 13:56:52 +0000

    added dark mode and light mode support and made the routing more efficient

--- frontend-react/merbanhub/app/layout.tsx
+++ frontend-react/merbanhub/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AppSidebar } from "@/components/app-sidebar";
 import { SidebarProvider } from "@/components/ui/sidebar";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,13 +26,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-    
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
         {children}
-
+</ThemeProvider>
       </body>
     </html>
   );
--- frontend-react/merbanhub/components/nav-main.tsx
+++ frontend-react/merbanhub/components/nav-main.tsx
@@ -11,6 +11,7 @@ import {
   SidebarMenuItem,
 } from "@/components/ui/sidebar"
 import Link from "next/link"
+import { useRouter } from "next/navigation"
 export function NavMain({
   items,
 }: {
@@ -20,6 +21,7 @@ export function NavMain({
     icon?: Icon
   }[]
 }) {
+  const route=useRouter()
   return (
     <SidebarGroup>
       <SidebarGroupContent className="flex flex-col gap-2">
@@ -45,13 +47,13 @@ export function NavMain({
         <SidebarMenu>
           {items.map((item) => (
             <SidebarMenuItem key={item.title}>
-              <Link href={item.url} className="w-full">
-              <SidebarMenuButton tooltip={item.title}>
+              <SidebarMenuButton tooltip={item.title} onClick={()=>route.push( item.url)} 
+              >
 
                 {item.icon && <item.icon />}
                 <span>{item.title}</span>
               </SidebarMenuButton>
-              </Link>
+              
             </SidebarMenuItem>
           ))}
         </SidebarMenu>
