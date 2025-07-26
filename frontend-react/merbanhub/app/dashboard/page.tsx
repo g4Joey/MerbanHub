ad2430354a888911541f8471d0d6bb03ed8e3428
Lucius Jackson <luciusjackson5@gmail.com>
2025-07-26 10:26:50 +0000

    removed the ui bloat in the dashboard page

--- frontend-react/merbanhub/app/dashboard/page.tsx
+++ frontend-react/merbanhub/app/dashboard/page.tsx
@@ -10,9 +10,9 @@ export default function DashboardOverviewPage() {
   return (
     <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
       <div className="px-4 lg:px-6">
-        <ChartAreaInteractive />
+        {/* <ChartAreaInteractive /> */}
       </div>
-      <DataTable data={data} />
+      {/* <DataTable data={data} /> */}
     </div>
   );
 }
